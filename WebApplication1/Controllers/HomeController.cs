610d351462431aad8102121dec28c3081f6dcde5
MaksItLab <maksim.ardintsev@mail.ru>
2024-02-14 15:53:08 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private static readonly List<Employee> _employees = new()
+		{
+			new Employee{ID = 1, FirstName = "Jonh", LastName = "Reboot", Patronymic = "Noogvre", Age = 34 },
+			new Employee{ID = 2, FirstName = "Rett", LastName = "Dewor", Patronymic = "Farenier", Age = 16 },
+			new Employee{ID = 3, FirstName = "Bettir", LastName = "Mertynol", Patronymic = "Bertyop", Age = 20 },
+		};
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public string ShowStr([FromQuery]string a)
+		{
+			return $"HI! {a}";
+		}
+
+		public IActionResult Employees() {
+			return View(_employees);
+		}
+	}
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddControllersWithViews(); 
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.MapGet("/throw", () => {
+	throw new ApplicationException("Ошибка!");
+});
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.Run();
+
